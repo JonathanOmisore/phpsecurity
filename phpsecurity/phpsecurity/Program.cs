4efee00047b0d18905ea7e131fe501ab0ae84a1f
Waqob <comprogger@Chillbot>
2015-12-12 21:15:46 -0500

    yes

---
+++ phpsecurity/phpsecurity/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+namespace phpsecurity
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string URI = "http://chillbot/security/";
+			Console.WriteLine ("Security");
+			string person = Console.ReadLine ();
+			string myParameters = "name="+person;
+
+			using (WebClient wc = new WebClient()) {
+				wc.Headers [HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+				string HtmlResult = wc.UploadString (URI, myParameters);
+				bool success = HtmlResult.Contains ("welcome jonathan");
+				bool ohno = HtmlResult.Contains ("gtfo");
+				if (success)
+					Console.WriteLine ("Welcome");
+				if (ohno) {
+					Console.WriteLine ("you are not allowed");
+				
+
+				}
+	
+			}
+
+		}
+
+	}}
